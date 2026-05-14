cf05e2b53354941c730f5e0160c36cdfa2b54fb0
aundraytafalla <aundraytafalla@mabinicolleges.edu.ph>
2026-05-14 16:54:22 +0800

    Untracked files

--- database/migrations/2026-05-13_allow_staff_profiles_without_users.sql
+++
@@ -1,5 +0,0 @@
--- Manual schema update for optional staff login accounts.
--- Run this once in environments where staff_profiles.user_id was created as NOT NULL.
-
-ALTER TABLE staff_profiles
-    MODIFY COLUMN user_id BIGINT NULL;
